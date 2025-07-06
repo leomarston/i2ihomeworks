3bd0372475fdb5f92016152fedbb8cc46569dcf8
leomarston <kitapkurduyagiz@gmail.com>
2025-07-07 01:50:06 +0300

    feat: Add Selenium login test project for hw3

---
+++ hw3/src/test/java/com/testautomation/LoginTest.java
@@ -0,0 +1,26 @@
+package com.testautomation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void testLogin() {
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement usernameField = driver.findElement(By.id("user-name"));
+        usernameField.sendKeys("standard_user");
+
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("secret_sauce");
+
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "https://www.saucedemo.com/inventory.html");
+    }
+} 
